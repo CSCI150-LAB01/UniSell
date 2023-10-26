c51f2c3870d5603c10542dcb8fc7fdad481ddc15
Leila Eshaghi <142948372+leilaeshrocks@users.noreply.github.com>
2023-10-25 18:04:53 -0700

    Adding default swift UI

---
+++ UniSell_FresnoState/UniSell_FresnoStateTests/UniSell_FresnoStateTests.swift
@@ -0,0 +1,33 @@
+//
+//  UniSell_FresnoStateTests.swift
+//  UniSell_FresnoStateTests
+//
+//  Created by Leila Eshaghi on 10/3/23.
+//
+
+import XCTest
+@testable import UniSell_FresnoState
+
+class UniSell_FresnoStateTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
