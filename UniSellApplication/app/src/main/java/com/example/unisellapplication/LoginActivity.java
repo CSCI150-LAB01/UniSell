b7d1f971147997e8a8844202a639496b8bde10bf
leilaeshrocks <eshaghi_leila@mail.fresnostate.edu>
2023-10-29 23:16:04 -0700

    Added OnClickListeners and Login Page

--- UniSellApplication/app/src/main/java/com/example/unisellapplication/LoginActivity.java
+++ UniSellApplication/app/src/main/java/com/example/unisellapplication/LoginActivity.java
@@ -2,13 +2,42 @@ package com.example.unisellapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-
+    Button signIn;
+    EditText email, password;
+    TextView signUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signIn = findViewById(R.id.login_button);
+        email = findViewById(R.id.email_login);
+        password = findViewById(R.id.password_login);
+        signUp = findViewById(R.id.sign_up_login);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,RegistrationActivity.class));
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
     }
 }
