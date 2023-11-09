53b93ce2cdd6e9171dfea0dcf558069c5ed8f448
I-Benavides <ibenas21@mail.fresnostate.edu>
2023-11-08 17:51:41 -0800

    Finished progress bar

--- UniSellApplication/app/src/main/java/com/example/unisellapplication/MainActivity.java
+++ UniSellApplication/app/src/main/java/com/example/unisellapplication/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
@@ -18,17 +19,24 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.unisellapplication.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     ProgressBar progressBar;
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_page);
 
+        auth = FirebaseAuth.getInstance();
         progressBar = findViewById(R.id.progressbar);
         progressBar.setVisibility(View.GONE);
+        if(auth.getCurrentUser() != null){
+            progressBar.setVisibility(View.VISIBLE);
+            Toast.makeText(this, "please wait you are already logged in", Toast.LENGTH_SHORT).show();
+        }
     }
     public void login(View view) {
         startActivity(new Intent(MainActivity.this, LoginActivity.class));
