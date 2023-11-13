22c24687d5e86d9d73d096fb5b965c7b9310fdf1
I-Benavides <ibenas21@mail.fresnostate.edu>
2023-11-12 19:42:50 -0800

    update add listing page

--- UniSellApplication/app/src/main/java/com/example/unisellapplication/ui/create_listing/CreateListingFragment.java
+++ UniSellApplication/app/src/main/java/com/example/unisellapplication/ui/create_listing/CreateListingFragment.java
@@ -1,5 +1,7 @@
 package com.example.unisellapplication.ui.create_listing;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -18,10 +20,33 @@ public class CreateListingFragment extends Fragment {
 
     ImageButton imageButton;
     EditText title, description, price;
+    private static final int SELECT_PICTURE = 200;
+
+//    FirebaseFirestore db;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_create_listing, container, false);
+
+        imageButton = root.findViewById(R.id.add_img_button);
+        title = root.findViewById(R.id.editTextTitle);
+        description = root.findViewById(R.id.editTextDescripiton);
+        price =root.findViewById(R.id.editTextPrice);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenGallery();
+            }
+        });
+
         return root;
+    }
+
+    private void OpenGallery() {
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE);
     }
 }
