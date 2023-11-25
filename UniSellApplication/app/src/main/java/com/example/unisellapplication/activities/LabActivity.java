22457b026b891879f506d5dd8024eb8684971464
leilaeshrocks <eshaghi_leila@mail.fresnostate.edu>
2023-11-24 16:18:38 -0800

    Category buttons

---
+++ UniSellApplication/app/src/main/java/com/example/unisellapplication/activities/LabActivity.java
@@ -0,0 +1,16 @@
+package com.example.unisellapplication.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.unisellapplication.R;
+
+public class LabActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab);
+    }
+}
