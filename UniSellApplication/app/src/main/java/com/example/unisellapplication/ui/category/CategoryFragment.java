2925e9f5d0b372b5a2f554f1d09107c8685325a1
I-Benavides <ibenas21@mail.fresnostate.edu>
2023-11-24 16:29:35 -0800

    added click event listeners

--- UniSellApplication/app/src/main/java/com/example/unisellapplication/ui/category/CategoryFragment.java
+++ UniSellApplication/app/src/main/java/com/example/unisellapplication/ui/category/CategoryFragment.java
@@ -11,44 +11,38 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.unisellapplication.R;
+import com.example.unisellapplication.activities.DormActivity;
+import com.example.unisellapplication.activities.TechnologyActivity;
 import com.example.unisellapplication.activities.TextbooksActivity;
 
 public class CategoryFragment extends Fragment {
-    LinearLayout textbook_layout;
+    LinearLayout textbook_layout, dorm_layout, tech_layout;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_category, container, false);
 
         textbook_layout = root.findViewById(R.id.ic_textbook_layout);
+        dorm_layout = root.findViewById(R.id.dorm_layout);
+        tech_layout = root.findViewById(R.id.tech_layout);
         textbook_layout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getActivity(), TextbooksActivity.class));
             }
         });
+        dorm_layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), DormActivity.class));
+            }
+        });
+        tech_layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), TechnologyActivity.class));
+            }
+        });
         return root;
     }
-
-
-//    public void login(View view) {
-//        startActivity(new Intent(HomeActivity.this, LoginActivity.class));
-//    }
-//
-//    public void login(View view) {
-//        startActivity(new Intent(HomeActivity.this, LoginActivity.class));
-//    }
-//
-//    public void login(View view) {
-//        startActivity(new Intent(HomeActivity.this, LoginActivity.class));
-//    }
-//
-//    public void login(View view) {
-//        startActivity(new Intent(HomeActivity.this, LoginActivity.class));
-//    }
-//
-//    public void login(View view) {
-//        startActivity(new Intent(HomeActivity.this, LoginActivity.class));
-//    }
-
 }
